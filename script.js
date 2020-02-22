f4dfebe028ee00f7675de72cefdde6e70bba5726
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2020-02-22 04:03:36 +0000

    Remove by dragging to bin

--- script.js
+++ script.js
@@ -18,19 +18,82 @@ $(document).ready(function(){
 	$("#foreground").sortable({
 		start: function(event, ui){
     	ui.helper.height("64px");
-    }
+    	showBin();
+    },
+    stop: function(event, ui){
+    	hideBin();
+    	if(event.target == $('#foreground')[0]){
+    		console.log("from foreground");
+    		removeToken(ui.item[0].id.slice(5));
+    	}
+    	//console.log(ui.item[0].id.slice(5));
+    },
+  	connectWith: "#trashBin",
+  	scroll: false
+  });
+
+  $("#trashBin").sortable({
+  	stop: function(event, ui){
+  		console.log(ui.item.id);
+  	},
+  	connectWith: "#foreground",
+  	scroll: false
   });
 
   $("#add").click(addToken);
   $("#cancel").click(hideInput);
+  $("#sortList").click(sortList);
+  $("#nextTurn").click(shuffleList);
 
 	console.log("init done");
 });
+
+function showBin(){
+	$("#trashBin").removeClass("hidden");
+}
+
+function hideBin(){
+	$("#trashBin").addClass("hidden");
+}
+
+function removeToken(id){
+	for(var i = 0; i < tokens.length; i++){
+		if(tokens[i].id == id){
+			tokens.splice(i, 1);
+			break;
+		}
+	}
+	redrawList();
+}
 
 function showInput(){
 	console.log("click");
 	$('#enterInitiative').removeClass("hidden");
 	$('#greyOut').removeClass("hidden");
+}
+
+function sortList(){
+	tokens.sort(function(a, b){
+		if(a.initiative < b.initiative){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	});
+	redrawList();
+}
+
+function shuffleList(){
+	tokens.push(tokens.shift());
+	redrawList();
+}
+
+function redrawList(){
+	$("#foreground")[0].innerHTML = "";
+	for(var i = 0; i < tokens.length; i++){
+		$("#foreground")[0].innerHTML += `<div id=token${tokens[i].id} class="token"><label class="tokenName">${tokens[i].name}</label><label class="tokenInitiative">${tokens[i].initiative}</label></div>`;
+	}
 }
 
 function hideInput(){
